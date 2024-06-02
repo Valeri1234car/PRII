36141576068b9694f9808c72d0739f73b9b8411c
Lazar Cvorovic <lazar.cvorovic@student.um.si>
2024-06-02 11:58:14 +0200

    Sprememba izgleda

--- stran/praktikum-2-app/src/components/Menu/Menu.tsx
+++ stran/praktikum-2-app/src/components/Menu/Menu.tsx
@@ -3,7 +3,7 @@
 const Menu = () =>{
 
     return (
-        <header className="header-container d-flex justify-content-between align-items-center p-3 bg-light">
+        <header className="header-container d-flex justify-content-between align-items-center ">
             <h1 className="header-title">MINILON</h1>
             <img src="/logo/logo.PNG" alt="Logo" className="header-logo" />
         </header>
