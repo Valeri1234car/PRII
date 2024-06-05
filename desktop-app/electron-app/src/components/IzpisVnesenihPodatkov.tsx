3429b51d27882a362c09d8be4bb9b990bca0428f
Lazar Cvorovic <lazar.cvorovic@student.um.si>
2024-06-05 16:35:44 +0200

    Prikaz podatkov v Excel

--- desktop-app/electron-app/src/components/IzpisVnesenihPodatkov.tsx
+++ desktop-app/electron-app/src/components/IzpisVnesenihPodatkov.tsx
@@ -1,139 +1,99 @@
-import { PodatkiContext } from "../App"
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { PodatkiContext } from "../App";
 import BonitetnaOcena from "./BonitetnaOcena";
+import * as XLSX from 'xlsx';
 
+const IzpisVnesenihPodatkov = () => {
+    const { podatkiState } = useContext(PodatkiContext);
+    const [tableData, setTableData] = useState([]);
 
-const IzpisVnesenihPodatkov = () =>{
+    const generateExcelData = () => {
+        const data = [
+            ["Osebni podakti"],
+            ["Ime", podatkiState.ime],
+            ["Priimek", podatkiState.priimek],
+            ["Naslov", podatkiState.naslov],
+            ["Datum rojstva", podatkiState.datumRojstva],
+            ["Starost", podatkiState.starost],
+            [],
+            ["Osnovni kriteriji"],
+            ["Državljan Republike Slovenije", podatkiState.drzavljanRS ? "Da" : "Ne"],
+            ["Starejši od 18 let", podatkiState.starost18 ? "Da" : "Ne"],
+            ["Prosilec NI v stečajnem postopku", podatkiState.stecajniPostopekNI ? "Da" : "Ne"],
+            ["Zaposlen ali upokojenec", podatkiState.zaposlenUpokojenec ? "Da" : "Ne"],
+            [],
+            ["Kredit Minilon"],
+            ["Zaprošeni znesek kredita v EUR", podatkiState.zaproseniKredit],
+            ["Rok vračila v mesecih", podatkiState.rokVracila],
+            ["Mesečna anuiteta", podatkiState.mesecnaAmuniteta],
+            [],
+            ["Podakti o zaposlitvi"],
+            ["Delodajalec", podatkiState.delodajalec],
+            ["Bonitetna ocena delodajalca", podatkiState.bonitetnaOcenaDelodajalca],
+            [],
+            ["Finančno poslovanje podakti"],
+            ["PODATKI O FINANČNEM POSLOVANJU V EUR", "T-3", "T-2", "T-1", "Povprečje"],
+            ["Mesečni promet v dobro", podatkiState.mesecniPrometDobro.t1, podatkiState.mesecniPrometDobro.t2, podatkiState.mesecniPrometDobro.t3, podatkiState.mesecniPrometDobro.povprecje],
+            ["Mesečni promet v breme", podatkiState.mesecniPrometBreme.t1, podatkiState.mesecniPrometBreme.t2, podatkiState.mesecniPrometBreme.t3, podatkiState.mesecniPrometBreme.povprecje],
+            ["Stanje na TRR", podatkiState.stanjeTRR.t1, podatkiState.stanjeTRR.t2, podatkiState.stanjeTRR.t3, podatkiState.stanjeTRR.povprecje],
+            ["Znesek prejemkov iz dela oz. pokojnina", podatkiState.znesekPrejemkovPokojnina.t1, podatkiState.znesekPrejemkovPokojnina.t2, podatkiState.znesekPrejemkovPokojnina.t3, podatkiState.znesekPrejemkovPokojnina.povprecje],
+            ["Znesek drugih prejemkov", podatkiState.znesekDrugihPrejemkov.t1, podatkiState.znesekDrugihPrejemkov.t2, podatkiState.znesekDrugihPrejemkov.t3, podatkiState.znesekDrugihPrejemkov.povprecje],
+            ["Mesečni znesek za odplačilo obstoječih kreditov", podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.t1, podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.t2, podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.t3, podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.povprecje],
+            ["Število neizvršenih trajnih nalogov", podatkiState.stNeizvrsenihTrajnihNalogov.t1, podatkiState.stNeizvrsenihTrajnihNalogov.t2, podatkiState.stNeizvrsenihTrajnihNalogov.t3, podatkiState.stNeizvrsenihTrajnihNalogov.povprecje],
+            ["Število bančnih pobotov", podatkiState.stBancnihPobotov.t1, podatkiState.stBancnihPobotov.t2, podatkiState.stBancnihPobotov.t3, podatkiState.stBancnihPobotov.povprecje],
+            ["Število izvršb na TRR", podatkiState.stIzvrsbNaTrr.t1, podatkiState.stIzvrsbNaTrr.t2, podatkiState.stIzvrsbNaTrr.t3, podatkiState.stIzvrsbNaTrr.povprecje],
+            [],
+            ["Nerubljiv dohodek v EUR", podatkiState.nerubljivDohodek],
+            ["Rubljivi dohodek v EUR", podatkiState.rubljivDohodek],
+            ["Dohodki po plačilu obstoječih finančnih obveznosti", podatkiState.dohodkiPoPlaciluStarga],
+            ["Dohodki po plačilu obstoječih in novih finančnih obveznosti", podatkiState.dohodkiPoPlaciluVsega],
+            [],
+            ["Drugi podatki"],
+            ["Izobrazba", podatkiState.izobrazba],
+            ["(So)Lastništvo nepremičnin", podatkiState.lastnistnovNepremicnin ? "Da" : "Ne"],
+            ["Število vzdrževanih družinskih članov", podatkiState.stVzdrzevanihDruzinskihClanov],
+            ["Ali je partner zaposlen?", podatkiState.partnerZaposlen ? "Da" : "Ne"],
+            ["Samohranilec", podatkiState.samohranilec ? "Da" : "Ne"],
+            ["Zavezanec za preživnino", podatkiState.zavezanecNaPrezivnin ? "Da" : "Ne"],
+            ["Znesek mesečne preživnine", podatkiState.znesekMesecnePrezivnine],
+            ["Sum na razne oblike odvisnosti", podatkiState.sumljivost ? "Da" : "Ne"],
+            [],
+            ["PODATKI SISBON (V EUR)"],
+            ["SISBON - NEODPLAČAN DEL OBVEZNOSTI", podatkiState.sisbonNeodplacanDelObvezost],
+            ["SISBON - ZAPADLI DOLG", podatkiState.sisbonZapadliDolg],
+            ["SISBON - IZTERJAVA", podatkiState.sisbonIzterjava],
+            ["SISBON - IZVRŠBA", podatkiState.sisbonIzvrsba],
+            ["SISBON - OMEJITEV UPORABE TRR", podatkiState.sisbonOmejitevTRR ? "Da" : "Ne"]
+        ];
 
-    const {podatkiState, setPodatkiState, setStran} = useContext(PodatkiContext);
-    
-    return(
+        setTableData(data);
+    };
+
+    return (
         <>
-        <div className="container" style={{ borderColor: 'black', color: 'black', background: "white" }}>
-            <h2>Osebni podakti:</h2>
-            <p className="mb-3">Ime: {podatkiState.ime}</p>
-            <p className="mb-3">Priimek: {podatkiState.priimek}</p>
-            <p className="mb-3">Naslov: {podatkiState.naslov}</p>
-            <p className="mb-3">Datum rojstva: {podatkiState.datumRojstva}</p>
-            <p className="mb-3">Starost: {podatkiState.starost}</p>
-            <h2>Osnovni kriteriji</h2>
-            <p className="mb-3">Državljan Republike Slovenije: {podatkiState.drzavljanRS ? "Da" : "Ne"}</p>
-            <p className="mb-3">Starejši od 18 let: {podatkiState.starost18 ? "Da" : "Ne"}</p>
-            <p className="mb-3">Prosilec NI v stečajnem postopku: {podatkiState.stecajniPostopekNI ? "Da" : "Ne"}</p>
-            <p className="mb-3">Zaposlen ali upokojenec: {podatkiState.zaposlenUpokojenec ? "Da" : "Ne"}</p>
-            <h2>Kredit Minilon</h2>
-            <p className="mb-3">Zaprošeni znesek kredita v EUR: {podatkiState.zaproseniKredit}</p>
-            <p className="mb-3">Rok vračila v mesecih: {podatkiState.rokVracila}</p>
-            <p className="mb-3">Mesečna anuiteta: {podatkiState.mesecnaAmuniteta}</p>
-            <h2>Podakti o zaposlitvi</h2>
-            <p className="mb-3">Delodajalec: {podatkiState.delodajalec}</p>
-            <p className="mb-3">Bonitetna ocena delodajalca: {podatkiState.bonitetnaOcenaDelodajalca}</p>
-            <h2>Finančno poslovanje podakti</h2>
-             <table className="table table-striped table-secondary" style={{ borderColor: 'secondary'}}>
-                <thead>
-                <tr>
-                    <th scope="col"><b>PODATKI O FINANČNEM POSLOVANJU V EUR</b></th>
-                    <th scope="col">T-3</th>
-                    <th scope="col">T-2</th>
-                    <th scope="col">T-1</th>
-                    <th scope="col">Povprečje</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Mesečni promet v dobro:</td>
-                    <td>{podatkiState.mesecniPrometDobro.t1}</td>
-                    <td>{podatkiState.mesecniPrometDobro.t2}</td>
-                    <td>{podatkiState.mesecniPrometDobro.t3}</td>
-                    <td>{podatkiState.mesecniPrometDobro.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Mesečni promet v breme:</td>
-                    <td>{podatkiState.mesecniPrometBreme.t1}</td>
-                    <td>{podatkiState.mesecniPrometBreme.t2}</td>
-                    <td>{podatkiState.mesecniPrometBreme.t3}</td>
-                    <td>{podatkiState.mesecniPrometBreme.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Stanje na TRR:</td>
-                    <td>{podatkiState.stanjeTRR.t1}</td>
-                    <td>{podatkiState.stanjeTRR.t2}</td>
-                    <td>{podatkiState.stanjeTRR.t3}</td>
-                    <td>{podatkiState.stanjeTRR.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Znesek prejemkov iz dela oz. pokojnina:</td>
-                    <td>{podatkiState.znesekPrejemkovPokojnina.t1}</td>
-                    <td>{podatkiState.znesekPrejemkovPokojnina.t2}</td>
-                    <td>{podatkiState.znesekPrejemkovPokojnina.t3}</td>
-                    <td>{podatkiState.znesekPrejemkovPokojnina.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Znesek drugih prejemkov:</td>
-                    <td>{podatkiState.znesekDrugihPrejemkov.t1}</td>
-                    <td>{podatkiState.znesekDrugihPrejemkov.t2}</td>
-                    <td>{podatkiState.znesekDrugihPrejemkov.t3}</td>
-                    <td>{podatkiState.znesekDrugihPrejemkov.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Mesečni znesek za odplačilo obstoječih kreditov:</td>
-                    <td>{podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.t1}</td>
-                    <td>{podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.t2}</td>
-                    <td>{podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.t3}</td>
-                    <td>{podatkiState.mesecniZnesekZaOdplacilodrugihKreditov.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Število neizvršenih trajnih nalogov:</td>
-                    <td>{podatkiState.stNeizvrsenihTrajnihNalogov.t1}</td>
-                    <td>{podatkiState.stNeizvrsenihTrajnihNalogov.t2}</td>
-                    <td>{podatkiState.stNeizvrsenihTrajnihNalogov.t3}</td>
-                    <td>{podatkiState.stNeizvrsenihTrajnihNalogov.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Število bančnih pobotov:</td>
-                    <td>{podatkiState.stBancnihPobotov.t1}</td>
-                    <td>{podatkiState.stBancnihPobotov.t2}</td>
-                    <td>{podatkiState.stBancnihPobotov.t3}</td>
-                    <td>{podatkiState.stBancnihPobotov.povprecje}</td>
-                </tr>
-                <tr>
-                    <td>Število izvršb na TRR:</td>
-                    <td>{podatkiState.stIzvrsbNaTrr.t1}</td>
-                    <td>{podatkiState.stIzvrsbNaTrr.t2}</td>
-                    <td>{podatkiState.stIzvrsbNaTrr.t3}</td>
-                    <td>{podatkiState.stIzvrsbNaTrr.povprecje}</td>
-                </tr>
-                </tbody>
-            </table>
-
-            <div className="container">
-                <p className="mb-0">Nerubljiv dohodek v EUR: {podatkiState.nerubljivDohodek}</p>
-                <p className="mb-0">Rubljivi dohodek v EUR: {podatkiState.rubljivDohodek}</p>
-                <p className="mb-0">Dohodki po plačilu obstoječih finančnih obveznosti (krediti, Leasing,…): {podatkiState.dohodkiPoPlaciluStarga}</p>
-                <p className="mb-0">Dohodki po plačilu obstoječih in novih finančnih obveznosti: {podatkiState.dohodkiPoPlaciluVsega}</p>
-
-                <h2>Drugi podatki</h2>
-                <p className="mb-0">Izobrazba: {podatkiState.izobrazba}</p>
-                <p className="mb-0">(So)Lastništvo nepremičnin: {podatkiState.lastnistnovNepremicnin ? "Da" : "Ne"}</p>
-                <p className="mb-0">Število vzdrževanih družinskih članov: {podatkiState.stVzdrzevanihDruzinskihClanov}</p>
-                <p className="mb-0">Ali je partner zaposlen?: {podatkiState.partnerZaposlen ? "Da" : "Ne" }</p>
-                <p className="mb-0">Samhranilec: {podatkiState.samohranilec  ? "Da" : "Ne"}</p>
-                <p className="mb-0">Zavezanec za preživnino: {podatkiState.zavezanecNaPrezivnin ? "Da" : "Ne"}</p>
-                <p className="mb-0">Znesek mesečne preživnine: {podatkiState.znesekMesecnePrezivnine}</p>
-                <p className="mb-0">Sum na razne oblike odvisnosti, sumljive prakse pri najemanju kredita: {podatkiState.sumljivost ? "Da" : "Ne"}</p>
-                <h2>PODATKI SISBON (V EUR)</h2>
-
-                <p className="mb-0">SISBON - NEODPLAČAN DEL OBVEZNOSTI: {podatkiState.sisbonNeodplacanDelObvezost}</p>
-                <p className="mb-0">SISBON - ZAPADLI DOLG: {podatkiState.sisbonZapadliDolg}</p>
-                <p className="mb-0">SISBON - IZTERJAVA: {podatkiState.sisbonIzterjava}</p>
-                <p className="mb-0">SISBON - IZVRŠBA: {podatkiState.sisbonIzvrsba}</p>
-                <p className="mb-0">SISBON - OMEJITEV UPORABE TRR: {podatkiState.sisbonOmejitevTRR ? "Da" : "Ne"}</p>
+            <div className="container" style={{ borderColor: 'black', color: 'black', background: "white" }}>
+                <div className="d-flex justify-content-center mb-3">
+                    <button onClick={generateExcelData} className="btn btn-primary">Prikaz podatkov v Excel-u</button>
+                </div>
+                <h2>Prikaz Excel podataka</h2>
+                {tableData.length > 0 && (
+                    <table className="table table-bordered   mb-4">
+                        <tbody>
+                        {tableData.map((row, rowIndex) => (
+                            <tr key={rowIndex}>
+                                {row.map((cell, cellIndex) => (
+                                    <td key={cellIndex}>{cell}</td>
+                                ))}
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
- </div>
 
- <BonitetnaOcena/>
- </>
-    )
-}
+            <BonitetnaOcena />
+        </>
+    );
+};
 
 export default IzpisVnesenihPodatkov;
