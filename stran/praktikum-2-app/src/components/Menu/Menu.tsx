a21fc37a2dca881bca867bfcb90b20910cbcc660
Lazar Cvorovic <lazar.cvorovic@student.um.si>
2024-06-06 22:41:15 +0200

    malenkosti

--- stran/praktikum-2-app/src/components/Menu/Menu.tsx
+++ stran/praktikum-2-app/src/components/Menu/Menu.tsx
@@ -4,8 +4,8 @@ const Menu = () =>{
 
     return (
         <header className="header-container d-flex justify-content-between align-items-center ">
-            <h1 className="header-title">MINILON</h1>
-            <img src="/logo/logo.PNG" alt="Logo" className="header-logo" />
+            <h1 className="header-title">BONITETNA OCENA</h1>
+            <img src="/logo/logo2.PNG" alt="Logo" className="header-logo" />
         </header>
     );
 }
