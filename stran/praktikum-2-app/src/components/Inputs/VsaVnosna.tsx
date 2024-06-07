064bae2a8562ae848cdf654a167694f32eb6cbf7
Dom3n <domen.drovenik@student.um.si>
2024-06-07 10:51:08 +0200

    dodan gumb za resetiranje stanja

--- stran/praktikum-2-app/src/components/Inputs/VsaVnosna.tsx
+++ stran/praktikum-2-app/src/components/Inputs/VsaVnosna.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import DrugiPodatki from "./DrugiPodatki";
 import FinancnoPoslovanje from "./FinancnoPoslovanje";
 import KreditMinilon from "./KreditMinilon";
@@ -9,12 +9,74 @@ import { PodatkiContext } from "../../App";
 import VnosnoPolje from "./PdfReader";
 import SisbonPodatki from "./SisbonPodatki";
 import FinancniIzpiski from "./FinancniIzpiski";
+import { Podatki } from "../../interface/Podatki";
+import { Promet } from "../../interface/Podatki";
+
+const initialPromet: Promet = {
+    t1: 0,
+    t2: 0,
+    t3: 0,
+    povprecje: 0
+};
+
+const initialPodatki: Podatki = {
+    ime: "",
+    priimek: "",
+    naslov: "",
+    datumRojstva: new Date(),
+    starost: 0,
+    drzavljanRS: false,
+    starost18: false,
+    stecajniPostopekNI: false,
+    zaposlenUpokojenec: false,
+    zaproseniKredit: 0,
+    rokVracila: 0,
+    mesecnaAmuniteta: 0,
+    delodajalec: "",
+    bonitetnaOcenaDelodajalca: 0,
+    mesecniPrometDobro: { ...initialPromet },
+    mesecniPrometBreme: { ...initialPromet },
+    stanjeTRR: { ...initialPromet },
+    znesekPrejemkovPokojnina: { ...initialPromet },
+    znesekDrugihPrejemkov: { ...initialPromet },
+    mesecniZnesekZaOdplacilodrugihKreditov: { ...initialPromet },
+    stNeizvrsenihTrajnihNalogov: { ...initialPromet },
+    stBancnihPobotov: { ...initialPromet },
+    stIzvrsbNaTrr: { ...initialPromet },
+    rubljiviDohodek: 0,
+    nerubljiviDohodek: 0,
+    dohodkiPoPlaciluStarga: 0,
+    dohodkiPoPlaciluVsega: 0,
+    izobrazba: "",
+    lastnistnovNepremicnin: false,
+    stVzdrzevanihDruzinskihClanov: 0,
+    partnerZaposlen: false,
+    samohranilec: false,
+    zavezanecNaPrezivnin: false,
+    znesekMesecnePrezivnine: 0,
+    sumljivost: false,
+    sisbonNeodplacanDelObvezost: 0,
+    sisbonZapadliDolg: 0,
+    sisbonIzterjava: 0,
+    sisbonIzvrsba: 0,
+    sisbonOmejitevTRR: false,
+};
 
 const VsaVnosna: React.FC = () => {
-    const { setStran } = useContext(PodatkiContext);
+
+        const {setStran, podatkiState, setPodatkiState} = useContext(PodatkiContext);
+        
+        const HandleClick = () =>{
+            
+            setPodatkiState(initialPodatki);
+        }
+    
 
     return (
         <div className="vsaVnosna">
+            <div className="izbrisiGumbContainer mb-4">
+                <div className="izbrisiGumb" id="neki" onClick={HandleClick}>Remove inputs</div>
+            </div>
             <div className="pravougaonik">
                 <OsebniPodatki />
             </div>
